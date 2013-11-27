de7fd4e6a6bfa459aec0f286cfb5b8758dc5ea71
Eric Peeters <ericpeeters@Erics-MacBook-Pro.local>
2013-11-27 12:04:11 +0100

    Added descriptional comments

--- Scripts/makiLoader.js
+++ Scripts/makiLoader.js
@@ -1,10 +1,12 @@
 ;
 (function($, window, document, undefined) {
+    // Create a global makiSettings array to fill with settings objects
     window.__makiSettings = [];
 
     /* jQuery.prototype.maki */
     $.fn.maki = function(overrides) {
         
+        // Make sure all the ajax requests are sync and nothing is cached
         $.ajaxSetup({
             async: false,
             cache: false
@@ -14,13 +16,14 @@
         if (!overrides) {
             var overrides = {};
         }
-
+        
+        // If contentSrc is filled in, make sure it is formatted correctly
         if (overrides.contentSrc) {
             var contentSrc = (settings.contentSrc.indexOf('/') == 0) ? settings.contentSrc : settings.path + settings.contentSrc;
             $.getScript(contentSrc);
         }
 
-        // Cache element context wrapped in jQuery
+        // Cache element context incl one wrapped in jQuery
         var el = this,
             $el = $(this);
 
@@ -36,9 +39,9 @@
             debug: false
         };
 
-        // Set codeView variable equal to the copyControls
+        // Set codeView variable equal to the copyControls if not defined
         overrides.codeView = (overrides.codeView) ? overrides.codeView : this.defaults.copyControls;
-        // Extend the settings with the overrides
+        // Merge the defaults and overrides in a settings variable
         var settings = $.extend({}, this.defaults, overrides);
 
         // If the contents of settings.content is an array or multiple, if so: concat them
@@ -66,15 +69,26 @@
             settings.path = settings.path.substr(1);
         }
 
+
+        /** 
+        @func       =loadMakiDependencies
+        @namespace  =jQuery.prototype.maki
+        @desc       Loads maki's dependencies
+        **/
         function loadMakiDependencies() {
+            // Load in maki.js
             $.getScript(settings.path + "maki.js");
+
+            // If debug is set to true, load in all depedencies individually
             if (settings.debug == true) {
                 $.getScript(settings.path + "prettify.js");
                 $.getScript(settings.path + "jquery.zencoding.js");
                 $.getScript(settings.path + "ZeroClipboard.js");
-            } else {
+            } else { // Load in a minified file of all dependencies
                 $.getScript(settings.path + "makiDependencies.min.js");
             }
+
+            // If contentSrc is filled in, load in that source
             if (settings.contentSrc) {
                 var contentSrc = (settings.contentSrc.indexOf('/') == 0) ? settings.contentSrc : settings.path + settings.contentSrc;
                 $.getScript(contentSrc);
@@ -84,24 +98,29 @@
         // Keep a property of whether maki is activated or not
         settings.init = false;
 
+        // If the hash is provided and it equals the hash that is passed in
         if (settings.hash && location.hash == settings.hash) {
             location.hash = "#maki-init";
             settings.init = true;
         }
 
+        // On hash changed, do some checks and initialize maki
         $(window).on('hashchange', function() {
             if (settings.hash && location.hash == settings.hash && settings.init == false) {
                 settings.init = true;
             }
 
+            // If hash location is #maki-init and it is initialized
             if (location.hash == "#maki-init" && settings.init == true) {
                 loadMakiDependencies();
                 makiJS.create(settings, $el);
+            // If hash location is NOT #maki-init yet it is set to be initialized, reload the browser
             } else if (location.hash !== "#maki-init" && settings.init == true) {
                 location.reload();
             }
         });
 
+        // if there is no hash property filled in, execute maki instantly
         if (!settings.hash) {
             loadMakiDependencies();
             makiJS.create(settings, $el);
